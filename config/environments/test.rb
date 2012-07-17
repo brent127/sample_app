bfc34519a7870f14a5a61789ed19dad2c7e18e5f
Brent Williams <brenthwilliams@gmail.com>
2012-07-16 23:02:04 -0400

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,5 +33,9 @@ SampleApp::Application.configure do
   config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :stderr# Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
